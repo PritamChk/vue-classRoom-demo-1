9b2381fb703c0c9aa13a31744a7fbf6570f8bbde
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-18 00:19:40 +0530

    blah

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="w-10 h-10 text-blue">
+  <div class="w-10 h-10 text-blue mx-auto">
     <HandIcon />
+    <ChevronDoubleDownIcon class="w-10 h-10 text-blue-500" />
+    <AnnotationIcon class="w-10 h-10 text-fuchsia-400" />
   </div>
-  <ChevronDoubleDownIcon class="w-10 h-10 text-blue-500" />
-  <AnnotationIcon class="w-10 h-10 text-fuchsia-400" />
 </template>
 
 <script>
-import { HandIcon, 
-ChevronDoubleDownIcon,
-AnnotationIcon
+import {
+  HandIcon,
+  ChevronDoubleDownIcon,
+  AnnotationIcon,
 } from "@heroicons/vue/outline";
 export default {
   name: "DropDown",
