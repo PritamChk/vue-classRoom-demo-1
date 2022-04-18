25b643d7287be0639e23c84de5053659d06293b2
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-18 21:49:30 +0530

    error fixed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <NavBar></NavBar>
-  <router-view class="w-screen h-screen"></router-view>
+  <NavBar />
+  <router-view class="w-screen h-screen" />
 </template>
 
 <script setup>
 import NavBar from "./components/NavBar.vue";
+
 </script>
 
 <style>
