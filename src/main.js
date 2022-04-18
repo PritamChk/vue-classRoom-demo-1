f67d198e5b58d894e3a903e4b0dc30902c0135d8
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-18 17:51:43 +0530

    VueKinesis installed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
+import VueKinesis from "vue-kinesis";
 import './css/style.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueKinesis).mount('#app')
